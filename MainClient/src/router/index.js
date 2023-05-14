daf2b932fa7a0aa53ada4597f615d164195cff15
ArielLopes888 <ariellopes888@gmail.com>
2023-05-14 18:58:15 -0300

    config routers

--- MainClient/src/router/index.js
+++ MainClient/src/router/index.js
@@ -5,9 +5,19 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue')
     },
     {
       path: '/about',

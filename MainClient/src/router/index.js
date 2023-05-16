4e7faa0ad9adb28b569f8bd86131156cd3e90d65
ArielLopes888 <ariellopes888@gmail.com>
2023-05-16 02:46:35 -0300

    add router

--- MainClient/src/router/index.js
+++ MainClient/src/router/index.js
@@ -20,6 +20,11 @@ const router = createRouter({
       component: () => import('../views/RegisterView.vue')
     },
     {
+      path: '/findpeople',
+      name: 'findpeople',
+      component: () => import('../views/FindPeopleView.vue')
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
